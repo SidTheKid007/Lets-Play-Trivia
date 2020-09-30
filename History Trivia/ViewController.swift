c540a6f2443ea5b5e1fd878e57bfb775eeab02e7
Siddhu K <siddhesvark@gmail.com>
2020-09-30 13:06:36 -0700

    Initial Commit

---
+++ History Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  History Trivia
+//
+//  Created by Siddhesvar Kannan on 9/30/20.
+//  Copyright © 2020 Siddhesvar Kannan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
